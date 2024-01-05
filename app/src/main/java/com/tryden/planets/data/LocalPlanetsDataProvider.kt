f81fc6c231a6146abbdf57e132f8d432ab90d939
Tyler Ryden <tryden11@gmail.com>
2024-01-04 20:49:02 -0500

    replaced default planet image in local data source

--- app/src/main/java/com/tryden/planets/data/LocalPlanetsDataProvider.kt
+++ app/src/main/java/com/tryden/planets/data/LocalPlanetsDataProvider.kt
@@ -15,7 +15,7 @@ object LocalPlanetsDataProvider {
                 id = 1,
                 titleResourceId = R.string.mercury,
                 subtitleResourceId = R.string.planets_list_subtitle,
-                imageResourceId = R.drawable.planet_default,
+                imageResourceId = R.drawable.mercury,
                 planetsImageBanner = R.drawable.space_banner,
                 planetDetails = R.string.planets_detail_text
             ),
@@ -23,7 +23,7 @@ object LocalPlanetsDataProvider {
                 id = 2,
                 titleResourceId = R.string.venus,
                 subtitleResourceId = R.string.planets_list_subtitle,
-                imageResourceId = R.drawable.planet_default,
+                imageResourceId = R.drawable.venus,
                 planetsImageBanner = R.drawable.space_banner,
                 planetDetails = R.string.planets_detail_text
             ),
@@ -31,7 +31,7 @@ object LocalPlanetsDataProvider {
                 id = 3,
                 titleResourceId = R.string.earth,
                 subtitleResourceId = R.string.planets_list_subtitle,
-                imageResourceId = R.drawable.planet_default,
+                imageResourceId = R.drawable.earth,
                 planetsImageBanner = R.drawable.space_banner,
                 planetDetails = R.string.planets_detail_text
             ),
@@ -39,7 +39,7 @@ object LocalPlanetsDataProvider {
                 id = 4,
                 titleResourceId = R.string.mars,
                 subtitleResourceId = R.string.planets_list_subtitle,
-                imageResourceId = R.drawable.planet_default,
+                imageResourceId = R.drawable.mars,
                 planetsImageBanner = R.drawable.space_banner,
                 planetDetails = R.string.planets_detail_text
             ),
@@ -47,7 +47,7 @@ object LocalPlanetsDataProvider {
                 id = 5,
                 titleResourceId = R.string.jupiter,
                 subtitleResourceId = R.string.planets_list_subtitle,
-                imageResourceId = R.drawable.planet_default,
+                imageResourceId = R.drawable.jupiter,
                 planetsImageBanner = R.drawable.space_banner,
                 planetDetails = R.string.planets_detail_text
             ),
@@ -55,7 +55,7 @@ object LocalPlanetsDataProvider {
                 id = 6,
                 titleResourceId = R.string.saturn,
                 subtitleResourceId = R.string.planets_list_subtitle,
-                imageResourceId = R.drawable.planet_default,
+                imageResourceId = R.drawable.saturn,
                 planetsImageBanner = R.drawable.space_banner,
                 planetDetails = R.string.planets_detail_text
             ),
@@ -63,7 +63,7 @@ object LocalPlanetsDataProvider {
                 id = 7,
                 titleResourceId = R.string.uranus,
                 subtitleResourceId = R.string.planets_list_subtitle,
-                imageResourceId = R.drawable.planet_default,
+                imageResourceId = R.drawable.uranus,
                 planetsImageBanner = R.drawable.space_banner,
                 planetDetails = R.string.planets_detail_text
             ),
@@ -71,7 +71,7 @@ object LocalPlanetsDataProvider {
                 id = 8,
                 titleResourceId = R.string.neptune,
                 subtitleResourceId = R.string.planets_list_subtitle,
-                imageResourceId = R.drawable.planet_default,
+                imageResourceId = R.drawable.neptune,
                 planetsImageBanner = R.drawable.space_banner,
                 planetDetails = R.string.planets_detail_text
             ),
@@ -79,7 +79,7 @@ object LocalPlanetsDataProvider {
                 id = 9,
                 titleResourceId = R.string.dwarf_planets,
                 subtitleResourceId = R.string.planets_list_subtitle,
-                imageResourceId = R.drawable.planet_default,
+                imageResourceId = R.drawable.pluto,
                 planetsImageBanner = R.drawable.space_banner,
                 planetDetails = R.string.planets_detail_text
         )
