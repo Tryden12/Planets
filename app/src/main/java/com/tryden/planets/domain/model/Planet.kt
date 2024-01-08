08de0ce0e0bb912e8eb83818ebbe9e30195b6cd3
Tyler Ryden <tryden11@gmail.com>
2024-01-08 15:00:47 -0500

    Removed nulls from Planet model (UI model)

--- app/src/main/java/com/tryden/planets/domain/model/Planet.kt
+++ app/src/main/java/com/tryden/planets/domain/model/Planet.kt
@@ -1,13 +1,13 @@
 package com.tryden.planets.domain.model
 
 data class Planet(
-    val id: Int,
-    val name: String,
-    val description: String,
-    val imgUrl: String,
-    val imgDescription: String,
-    val wikiLink: String,
-    val planetOrder: Int,
-    val mass: String,
-    val volume: String
+    val id: Int = 0,
+    val name: String = "",
+    val description: String = "",
+    val imgUrl: String = "",
+    val imgDescription: String = "",
+    val wikiLink: String = "",
+    val planetOrder: Int = 0,
+    val mass: String = "",
+    val volume: String = "",
 )
