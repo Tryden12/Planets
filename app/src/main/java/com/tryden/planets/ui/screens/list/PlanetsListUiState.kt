7bf9db5261891dd60813699958751b14a3af4719
Tyler Ryden <tryden11@gmail.com>
2024-01-08 15:02:18 -0500

    PlanetsListUiState properly setup

--- app/src/main/java/com/tryden/planets/ui/screens/list/PlanetsListUiState.kt
+++ app/src/main/java/com/tryden/planets/ui/screens/list/PlanetsListUiState.kt
@@ -1,12 +1,14 @@
 package com.tryden.planets.ui.screens.list
 
 import com.tryden.planets.data.remote.dto.PlanetDto
+import com.tryden.planets.domain.model.Planet
 
 /**
  * UI state for the Home screen
  */
-sealed interface PlanetsListUiState {
-    data class Success(val planets: List<PlanetDto>): PlanetsListUiState
-    object Error: PlanetsListUiState
-    object Loading: PlanetsListUiState
-}
+data class PlanetsListUiState(
+    var planets: List<Planet> = emptyList(),
+    val isLoading: Boolean = true,
+    var currentPlanet: Planet = Planet(),
+    val isShowingListPage: Boolean = true
+)
