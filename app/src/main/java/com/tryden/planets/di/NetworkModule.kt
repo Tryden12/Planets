96c8ad86a26c7e189b0b5f521a3f5b799adb7c8d
Tyler Ryden <tryden11@gmail.com>
2024-01-09 09:44:44 -0500

    NetworkModule complete -> migrated base url provides to NetworkModule

--- app/src/main/java/com/tryden/planets/di/NetworkModule.kt
+++ app/src/main/java/com/tryden/planets/di/NetworkModule.kt
@@ -1,41 +1,62 @@
 package com.tryden.planets.di
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.tryden.planets.data.remote.RemoteDataSource
 import com.tryden.planets.data.remote.RemoteSource
 import com.tryden.planets.data.remote.service.PlanetsApiService
+import com.tryden.planets.utils.Constants
 import com.tryden.planets.utils.MyInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 /**
- * This is the Hilt NetworkModule class which provides the retrofit, client, service and repositories dependencies.
+ * This is the Hilt NetworkModule class which provides the following dependencies:
+ * retrofit, client, service and repositories
  */
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+    /**
+     * Retrofit singleton.
+     * Params: [url] [okHttpClient] [moshi] provided as singletons below.
+     */
     @Provides
     @Singleton
-    fun provideRetrofit(client: OkHttpClient, url: String): Retrofit {
+    fun provideRetrofit(url: String, okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
         return Retrofit.Builder()
             .baseUrl(url)
-            .client(client)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
+
+    @Provides
+    @Singleton
+    fun provideUrl(): String {
+        return Constants.BASE_URL
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
 
     @Provides
     @Singleton
     fun createClient(): OkHttpClient {
         val client = OkHttpClient.Builder().apply {
             addInterceptor(MyInterceptor())
+//            interceptors().add(MyInterceptor())
         }.build()
         return client
     }
