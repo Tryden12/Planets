28bb4e47cc9e4050bf984c1dc6fbfb72d709d546
Tyler Ryden <tryden11@gmail.com>
2024-01-09 09:48:39 -0500

    removed old commented out code

--- app/src/main/java/com/tryden/planets/ui/PlanetsAppOld.kt
+++ app/src/main/java/com/tryden/planets/ui/PlanetsAppOld.kt
@@ -14,6 +14,7 @@ import com.tryden.planets.R
 import com.tryden.planets.ui.appbar.PlanetsAppBar
 import com.tryden.planets.ui.screens.detail.PlanetsDetail
 import com.tryden.planets.ui.screens.list.PlanetsList
+import com.tryden.planets.ui.screens.list.PlanetsListViewModel
 import com.tryden.planets.ui.screens.listAndDetail.PlanetsListAndDetail
 import com.tryden.planets.utils.PlanetsContentType
 
@@ -21,62 +22,62 @@ import com.tryden.planets.utils.PlanetsContentType
  * Main composable that serves as a container
  * which displays content according to [uiState] and [windowSize]
  */
-@Composable
-fun PlanetsAppOld(
-    windowSize: WindowWidthSizeClass,
-    onBackPressed: () -> Unit
-) {
-    // We utilizing a ViewModel.Factory for manual DI
-    val viewModel: PlanetsViewModel = viewModel(factory = PlanetsViewModel.Factory)
-    val uiState by viewModel.uiState.collectAsState()
-    val contentType = when (windowSize) {
-        WindowWidthSizeClass.Compact,
-        WindowWidthSizeClass.Medium -> PlanetsContentType.ListOnly
-        WindowWidthSizeClass.Expanded -> PlanetsContentType.ListAndDetail
-        else -> PlanetsContentType.ListOnly
-    }
-
-    Scaffold(
-        topBar = {
-            PlanetsAppBar(
-                onBackButtonClick = { viewModel.navigateToListPage() },
-                isShowingListPage = uiState.isShowingListPage,
-                windowSize = windowSize
-            )
-        }
-    ) { innerPadding ->
-        if (contentType == PlanetsContentType.ListAndDetail) {
-             PlanetsListAndDetail(
-                 planetLocals = uiState.planetsList,
-                 selectedPlanetLocal = uiState.currentPlanetLocal,
-                 onClick = {
-                     viewModel.updateCurrentPlanet(it)
-                 },
-                 onBackPressed = onBackPressed,
-                 contentPadding = innerPadding,
-                 modifier = Modifier.fillMaxWidth()
-             )
-        } else {
-            if (uiState.isShowingListPage) {
-                PlanetsList(
-                    planetLocals = uiState.planetsList,
-                    onClick = {
-                        viewModel.updateCurrentPlanet(it)
-                        viewModel.navigateToDetailPage()
-                    },
-                    modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.padding_medium)),
-                    contentPadding = innerPadding
-                )
-            } else {
-                 PlanetsDetail(
-                     selectedPlanetLocal = uiState.currentPlanetLocal,
-                     contentPadding = innerPadding,
-                     onBackPressed = {
-                         viewModel.navigateToListPage()
-                     }
-                 )
-            }
-        }
-    }
-}
-
+//@Composable
+//fun PlanetsAppOld(
+//    windowSize: WindowWidthSizeClass,
+//    onBackPressed: () -> Unit
+//) {
+//    // We utilizing a ViewModel.Factory for manual DI
+//    val viewModel: PlanetsListViewModel = viewModel(factory = PlanetsViewModel.Factory)
+//    val uiState by viewModel.uiState.collectAsState()
+//    val contentType = when (windowSize) {
+//        WindowWidthSizeClass.Compact,
+//        WindowWidthSizeClass.Medium -> PlanetsContentType.ListOnly
+//        WindowWidthSizeClass.Expanded -> PlanetsContentType.ListAndDetail
+//        else -> PlanetsContentType.ListOnly
+//    }
+//
+//    Scaffold(
+//        topBar = {
+//            PlanetsAppBar(
+//                onBackButtonClick = { viewModel.navigateToListPage() },
+//                isShowingListPage = uiState.isShowingListPage,
+//                windowSize = windowSize
+//            )
+//        }
+//    ) { innerPadding ->
+//        if (contentType == PlanetsContentType.ListAndDetail) {
+//             PlanetsListAndDetail(
+//                 planetLocals = uiState.planetsList,
+//                 selectedPlanetLocal = uiState.currentPlanetLocal,
+//                 onClick = {
+//                     viewModel.updateCurrentPlanet(it)
+//                 },
+//                 onBackPressed = onBackPressed,
+//                 contentPadding = innerPadding,
+//                 modifier = Modifier.fillMaxWidth()
+//             )
+//        } else {
+//            if (uiState.isShowingListPage) {
+//                PlanetsList(
+//                    planetLocals = uiState.planetsList,
+//                    onClick = {
+//                        viewModel.updateCurrentPlanet(it)
+//                        viewModel.navigateToDetailPage()
+//                    },
+//                    modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.padding_medium)),
+//                    contentPadding = innerPadding
+//                )
+//            } else {
+//                 PlanetsDetail(
+//                     selectedPlanetLocal = uiState.currentPlanetLocal,
+//                     contentPadding = innerPadding,
+//                     onBackPressed = {
+//                         viewModel.navigateToListPage()
+//                     }
+//                 )
+//            }
+//        }
+//    }
+//}
+//
--- app/src/main/java/com/tryden/planets/ui/PlanetsViewModel.kt
+++ app/src/main/java/com/tryden/planets/ui/PlanetsViewModel.kt
@@ -10,7 +10,6 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.tryden.planets.PlanetsApplication
 import com.tryden.planets.data.local.LocalPlanetsDataProvider
-import com.tryden.planets.data.PlanetsRepository
 import com.tryden.planets.domain.model.PlanetLocal
 import com.tryden.planets.ui.screens.detail.PlanetDetailUiState
 import com.tryden.planets.ui.screens.list.PlanetsListUiState
@@ -26,117 +25,8 @@ import java.io.IOException
 /**
  * View Model for Planets app
  */
-class PlanetsViewModel(
-    private val planetsRepository: PlanetsRepository
-) : ViewModel() {
-
-    /**
-     * Factory for [PlanetsViewModel] that takes [PlanetsRepository] as a dependency
-     */
-    companion object {
-        val Factory: ViewModelProvider.Factory = viewModelFactory {
-            initializer {
-                val application = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PlanetsApplication)
-                val planetsRepository = application.container.planetsRepository
-                PlanetsViewModel(planetsRepository = planetsRepository)
-            }
-        }
-    }
-
-    /** The mutable State that stores the status of the most recent request */
-
-//    private val _planetsUiState = MutableStateFlow(PlanetsUiState.Loading)
-//    val planetsUiState: StateFlow<PlanetsUiState> = _planetsUiState
-
-    var planetsListUiState: PlanetsListUiState by mutableStateOf(PlanetsListUiState.Loading)
-        private set
-
-    var planetDetailUiState: PlanetDetailUiState by mutableStateOf(PlanetDetailUiState.Loading)
-        private set
-
-    var planetsListAndDetailUiState: PlanetListAndDetailUiState by mutableStateOf(
-        PlanetListAndDetailUiState.Loading
-    )
-        private set
+class PlanetsViewModel : ViewModel() {
 
 
-    /**
-     * Call getAllPlanets() on init so we can display status immediately.
-     */
-    init {
-        getAllPlanets()
-
-        // TODO: add getPlanet() if PlanetListAndDetailScreen is showing
-    }
-
-    /**
-     * Gets planets information from the Planets API Retrofit service and updates the
-     * [Planet] [List] [MutableList].
-     */
-    fun getAllPlanets() {
-        viewModelScope.launch {
-            planetsListUiState = PlanetsListUiState.Loading
-            planetsListUiState = try {
-                val planets = planetsRepository.getAllPlanets()
-                PlanetsListUiState.Success(planets)
-            } catch (e: IOException) {
-                PlanetsListUiState.Error
-            } catch (e: HttpException) {
-                PlanetsListUiState.Error
-            }
-
-        }
-    }
-
-    /**
-     * Gets planet detail information from the Planets API Retrofit service and updates the
-     * [Planet].
-     */
-    fun getPlanet() {
-        viewModelScope.launch {
-            planetDetailUiState = PlanetDetailUiState.Loading
-            planetDetailUiState = try {
-                PlanetDetailUiState.Success(planetsRepository.getPlanet())
-            } catch (e: IOException) {
-                PlanetDetailUiState.Error
-            } catch (e: HttpException) {
-                PlanetDetailUiState.Error
-            }
-        }
-    }
-
-
-    /**
-     * Deprecated: PlanetsViewModel utilizing LocalPlanetsDataProvider
-     */
-    private val _uiStateOld = MutableStateFlow(
-        PlanetsUiStateOld(
-            planetsList = LocalPlanetsDataProvider.getPlanetsData(),
-            currentPlanetLocal = LocalPlanetsDataProvider.getPlanetsData().getOrElse(0) {
-                LocalPlanetsDataProvider.defaultPlanet
-            }
-        )
-    )
-    val uiStateOld: StateFlow<PlanetsUiStateOld> = _uiStateOld
-
-    fun updateCurrentPlanet(selectedPlanetLocal: PlanetLocal) {
-        _uiStateOld.update {
-            it.copy(
-                currentPlanetLocal = selectedPlanetLocal
-            )
-        }
-    }
-
-    fun navigateToListPage() {
-        _uiStateOld.update {
-            it.copy(isShowingListPage = true)
-        }
-    }
-
-    fun navigateToDetailPage() {
-        _uiStateOld.update {
-            it.copy(isShowingListPage = false)
-        }
-    }
 
 }
