901f57579b83b8d12602d4b12bc0a22475f245af
Tyler Ryden <tryden11@gmail.com>
2024-01-12 09:19:55 -0500

    PlanetDetailScreen UI update

--- app/src/main/java/com/tryden/planets/ui/screens/detail/PlanetsDetailScreen.kt
+++ app/src/main/java/com/tryden/planets/ui/screens/detail/PlanetsDetailScreen.kt
@@ -2,31 +2,28 @@ package com.tryden.planets.ui.screens.detail
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -34,14 +31,14 @@ import coil.request.ImageRequest
 import com.example.compose.PlanetsTheme
 import com.tryden.planets.R
 import com.tryden.planets.domain.model.Planet
-
+import com.tryden.planets.utils.PlanetsContentType
 
 
 @Composable
 fun PlanetsDetailScreen(
     planet: Planet,
     onBackPressed: () -> Unit,
-    contentPadding: PaddingValues,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
     modifier: Modifier = Modifier
 ) {
     BackHandler {
@@ -49,192 +46,115 @@ fun PlanetsDetailScreen(
     }
     val scrollState = rememberScrollState()
     val layoutDirection = LocalLayoutDirection.current
-    // Entire screen box
-    Box(
-       modifier = modifier
-           .fillMaxWidth()
-           .verticalScroll(state = scrollState)
-           .padding(top = contentPadding.calculateTopPadding())
-           .height(400.dp) // TODO: Fix height issue for screen sizes
+    val horizontalPadding = dimensionResource(id = R.dimen.padding_detail_content_horizontal)
+    val verticalPadding = dimensionResource(id = R.dimen.padding_detail_content_vertical)
 
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    bottom = contentPadding.calculateTopPadding(),
-                    start = contentPadding.calculateStartPadding(layoutDirection),
-                    end = contentPadding.calculateEndPadding(layoutDirection)
-                )
-        ) {
-            // Top column for image and planet name
-            Column(
-                modifier = modifier
-                    .background(color = colorResource(id = R.color.black))
-                    .fillMaxWidth()
-            ) {
 
-                Box(
-                    modifier = Modifier.fillMaxWidth()
-//                    .height(400.dp)
-                ) {
-                    AsyncImage(
-                        model = ImageRequest.Builder(context = LocalContext.current)
-                            .data(planet.imgUrl)
-                            .crossfade(true)
-                            .build(),
-                        contentScale = ContentScale.Inside,
-                        contentDescription = planet.description,
-                        alignment = Alignment.TopCenter,
-                        error = painterResource(id = R.drawable.ic_broken_image),
-                        placeholder = painterResource(id = R.drawable.loading_img),
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Column(
-                        Modifier
-                            .align(Alignment.BottomStart)
-                            .fillMaxWidth()
-                            .background(
-                                Brush.verticalGradient(
-                                    listOf(Color.Transparent, MaterialTheme.colorScheme.scrim),
-                                    0f,
-                                    400f
-                                )
-                            )
-                    ) {
-                        Text(
-                            text = planet.name,
-                            style = MaterialTheme.typography.headlineLarge,
-                            color = MaterialTheme.colorScheme.inverseOnSurface,
-                            modifier = Modifier
-                                .padding(horizontal = dimensionResource(id = R.dimen.padding_small))
-                        )
-                    }
-                }
-            }
-            // Planet info
-            Text(
-                text = planet.description,
-                style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier.padding(
-                    vertical = dimensionResource(id = R.dimen.padding_detail_content_vertical),
-                    horizontal = dimensionResource(id = R.dimen.padding_detail_content_horizontal)
-                )
+    // Parent column
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+//            .verticalScroll(state = scrollState)
+            .padding(
+                top = contentPadding.calculateTopPadding(),
+                bottom = contentPadding.calculateTopPadding(),
+                start = contentPadding.calculateStartPadding(layoutDirection),
+                end = contentPadding.calculateEndPadding(layoutDirection)
             )
-        }
-    }
-}
-
-@Composable
-fun PlanetDetailImageBox(
-    planet: Planet,
-    modifier: Modifier = Modifier
-) {
-    Box {
-        Box(
-            Modifier.background(color = colorResource(id = R.color.black))
-        ){
+    ) {
+        // Planet Image
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(colorResource(id = R.color.black))
+        ) {
             AsyncImage(
                 model = ImageRequest.Builder(context = LocalContext.current)
                     .data(planet.imgUrl)
                     .crossfade(true)
                     .build(),
-                contentScale = ContentScale.FillWidth,
+                contentScale = ContentScale.Inside,
                 contentDescription = planet.description,
-                alignment = Alignment.TopCenter,
+                alignment = Alignment.Center,
                 error = painterResource(id = R.drawable.ic_broken_image),
                 placeholder = painterResource(id = R.drawable.loading_img),
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
             )
-
         }
+        // Planet info
         Column(
             Modifier
-                .align(Alignment.BottomStart)
                 .fillMaxWidth()
-                .background(
-                    Brush.verticalGradient(
-                        listOf(Color.Transparent, MaterialTheme.colorScheme.scrim),
-                        0f,
-                        400f
-                    )
+                .background(colorResource(id = R.color.black))
+                .padding(
+                    horizontal = horizontalPadding,
+                    vertical = verticalPadding
                 )
         ) {
+            // Planet name
             Text(
                 text = planet.name,
                 style = MaterialTheme.typography.headlineLarge,
                 color = MaterialTheme.colorScheme.inverseOnSurface,
                 modifier = Modifier
-                    .padding(horizontal = dimensionResource(id = R.dimen.padding_small))
             )
+            // Planet description
+            Text(
+                text = planet.description,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.inverseOnSurface,
+                textAlign = TextAlign.Left,
+                modifier = Modifier
+
+            )
+            Column {
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // Planet mass
+                    Text(
+                        text = stringResource(id = R.string.mass),
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.inverseOnSurface,
+                        modifier = Modifier.weight(.2f)
+
+                    )
+                    Text(
+                        text = planet.mass,
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.inverseOnSurface,
+                        textAlign = TextAlign.Left,
+                        modifier = Modifier.weight(.8f).padding(horizontal = horizontalPadding)
+                    )
+                }
+            }
+            Column {
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    // Planet volume
+                    Text(
+                        text = stringResource(id = R.string.volume),
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.inverseOnSurface,
+                        modifier = Modifier.weight(.2f)
+
+                    )
+                    Text(
+                        text = planet.volume,
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.inverseOnSurface,
+                        modifier = Modifier.weight(.8f).padding(horizontal = horizontalPadding)
+                    )
+                }
+            }
         }
     }
-}
 
-@Composable
-fun PlanetDetailInfo(
-    planet: Planet,
-    modifier: Modifier = Modifier
-) {
-//    Row(
-//        modifier = modifier
-//            .fillMaxWidth()
-//            .height(50.dp)
-//            .background(MaterialTheme.colorScheme.onSurfaceVariant, shape = RectangleShape),
-//        horizontalArrangement = Arrangement.Start,
-//        verticalAlignment = Alignment.CenterVertically,
-//    ) {
-//        Column(modifier = modifier){
-//            Text(
-//                text = stringResource(id = R.string.mass),
-//                style = MaterialTheme.typography.bodyLarge,
-//                color = MaterialTheme.colorScheme.onPrimary,
-//                modifier = Modifier.padding(
-//                    vertical = dimensionResource(id = R.dimen.padding_detail_content_vertical),
-//                    horizontal = dimensionResource(id = R.dimen.padding_detail_content_horizontal)
-//                )
-//            )
-//            Text(
-//                text = planet.mass,
-//                style = MaterialTheme.typography.bodyMedium,
-//                color = MaterialTheme.colorScheme.onPrimary,
-//                modifier = Modifier.padding(
-//                    vertical = dimensionResource(id = R.dimen.padding_detail_content_vertical),
-//                    horizontal = dimensionResource(id = R.dimen.padding_detail_content_horizontal)
-//                )
-//            )
-//        }
-//        Column(
-//            modifier = modifier
-//        ){
-//            Text(
-//                text = stringResource(id = R.string.volume),
-//                style = MaterialTheme.typography.bodyLarge,
-//                color = MaterialTheme.colorScheme.onPrimary,
-//                modifier = Modifier.padding(
-//                    vertical = dimensionResource(id = R.dimen.padding_detail_content_vertical),
-//                    horizontal = dimensionResource(id = R.dimen.padding_detail_content_horizontal)
-//                )
-//            )
-//            Text(
-//                text = planet.volume,
-//                style = MaterialTheme.typography.bodyMedium,
-//                color = MaterialTheme.colorScheme.onPrimary,
-//                modifier = Modifier.padding(
-//                    vertical = dimensionResource(id = R.dimen.padding_detail_content_vertical),
-//                    horizontal = dimensionResource(id = R.dimen.padding_detail_content_horizontal)
-//                )
-//            )
-//        }
-//    }
-    Text(
-        text = planet.description,
-        style = MaterialTheme.typography.bodyMedium,
-        modifier = Modifier.padding(
-            vertical = dimensionResource(id = R.dimen.padding_detail_content_vertical),
-            horizontal = dimensionResource(id = R.dimen.padding_detail_content_horizontal)
-        )
-    )
+
 }
 
 @Preview
